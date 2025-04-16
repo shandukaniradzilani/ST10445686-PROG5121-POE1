c8125511659a27d29d3085274d2c53e461d36985
Shandukani Radzilani <165278788+shandukaniradzilani@users.noreply.github.com>
2025-04-16 23:23:23 +0200

    update

---
+++ src/poe1/POE1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poe1;
+
+/**
+ *
+ * @author radzi
+ */
+import java.util.Scanner;
+public class POE1 {
+
+private String userN;
+private int passW;
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Regristration/Login");
+        
+        
+        
+    }
+    
+}
