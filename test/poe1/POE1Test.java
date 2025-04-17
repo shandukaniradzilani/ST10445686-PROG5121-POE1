a970e8676766dccdf0e3d2208de99e77fcd4a5b8
Shandukani Radzilani <165278788+shandukaniradzilani@users.noreply.github.com>
2025-04-17 10:31:53 +0200

    update commit

--- test/poe1/POE1Test.java
+++ test/poe1/POE1Test.java
@@ -3,49 +3,77 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 package poe1;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author radzi
- */
-public class POE1Test {
-    
-    public POE1Test() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
+class POE1Test {
+    private Login login;
+
     @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+    void setUp() {
+        login = new Login();
     }
 
-    /**
-     * Test of main method, of class POE1.
-     */
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        POE1.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void testValidateUsername_Valid() {
+        assertTrue(login.validateUsername("abc_1"));
     }
-    
+
+    @Test
+    void testValidateUsername_InvalidNoUnderscore() {
+        assertFalse(login.validateUsername("abcde"));
+    }
+
+    @Test
+    void testValidateUsername_InvalidTooLong() {
+        assertFalse(login.validateUsername("abcde_1"));
+    }
+
+    @Test
+    void testValidatePassword_Valid() {
+        assertTrue(login.validatePassword("Passw0rd!"));
+    }
+
+    @Test
+    void testValidatePassword_TooShort() {
+        assertFalse(login.validatePassword("Pa0!"));
+    }
+
+    @Test
+    void testValidatePassword_NoSpecialChar() {
+        assertFalse(login.validatePassword("Password1"));
+    }
+
+    @Test
+    void testValidatePassword_NoNumber() {
+        assertFalse(login.validatePassword("Password!"));
+    }
+
+    @Test
+    void testValidatePassword_NoUppercase() {
+        assertFalse(login.validatePassword("password1!"));
+    }
+
+    @Test
+    void testValidatePhoneNumber_Valid() {
+        assertTrue(login.validatePhoneNumber("+27821234567"));
+    }
+
+    @Test
+    void testValidatePhoneNumber_InvalidPrefix() {
+        assertFalse(login.validatePhoneNumber("0821234567"));
+    }
+
+    @Test
+    void testRegisterAndLogin_Success() {
+        login.register("abc_1", "Passw0rd!", "+27821234567");
+        assertTrue(login.login("abc_1", "Passw0rd!"));
+    }
+
+    @Test
+    void testLogin_InvalidCredentials() {
+        login.register("abc_1", "Passw0rd!", "+27821234567");
+        assertFalse(login.login("abc_1", "WrongPass1!"));
+    }
 }
